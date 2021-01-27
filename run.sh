d486f63af6234cab544209117c2669dc4280025c
Robin Raymond <robin@robinraymond.de>
2021-01-27 18:22:10 +0100

    Lint bash (#1)

--- run.sh
+++ run.sh
@@ -1,24 +1,26 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 # inputs given by GitHub action -e docker runtime env vars
 TKTL_API_KEY=$INPUT_TKTL_API_KEY
 COMMIT_SHA=$GITHUB_SHA
 
 # Check
-tktl login $TKTL_API_KEY
+tktl login "$TKTL_API_KEY"
 
-until tktl get deployments -c $COMMIT_SHA ; do
+until tktl get deployments -c "$COMMIT_SHA" ; do
     sleep 10
     echo 'Waiting for public docs URL ...'
 done
 
-echo "Public docs exists"
+echo 'Public docs exists'
 
-while tktl get deployments -c $COMMIT_SHA -f -O json | jq '.[].status' | grep -v -q 'running'; do
+while tktl get deployments -c "$COMMIT_SHA" -f -O json | jq '.[].status' | grep -v -q 'running'; do
     sleep 2
-    echo "Waiting for deployment status to be running ..."
+    echo 'Waiting for deployment status to be running ...'
 done
 
-echo "The status is now ..."
-echo $(tktl get deployments -c $COMMIT_SHA -f -O json | jq '.[].status' )
-echo "Deployment is live!"
+echo 'The status is now ...'
+tktl get deployments -c "$COMMIT_SHA" -f -O json | jq '.[].status'
+echo 'Deployment is live!'
