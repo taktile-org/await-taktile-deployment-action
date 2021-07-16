2cfcf908db191c27a951d66fdf6e18848bbc612c
Jonas Nelle <jonas.nelle@taktile.com>
2021-07-16 13:47:06 +0200

    update language

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ tktl login "$TKTL_API_KEY"
 
 while tktl get deployments -c "$COMMIT_SHA" -f -O json | jq '.[].status' | grep -vE -q 'running|profiling'; do
     sleep 2
-    echo 'Waiting for deployment to be complete (status profiling or running) ...'
+    echo 'Waiting for deployment to complete (status profiling or running) ...'
 done
 
 echo 'The status is now ...'
